738cff6ec5d31319907314c273ab6cb34618fe62
Maxime Pelletier <maxpelle@c1r8p10.42quebec.com>
2023-12-06 12:36:31 -0500

    Make camera orientation work

--- src/camera.c
+++ src/camera.c
@@ -6,7 +6,7 @@
 /*   By: maxpelle <maxpelle@student.42quebec.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 08:34:22 by maxpelle          #+#    #+#             */
-/*   Updated: 2023/12/06 11:58:17 by maxpelle         ###   ########.fr       */
+/*   Updated: 2023/12/06 12:28:46 by maxpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	mrt_create_camera(t_data *data)
 	ref_y.y = 1;
 	ref_y.z = 0;
 	data->camera.z_axis = vnormalize(data->camera.z_axis);
-	data->camera.x_axis = vcross(data->camera.z_axis, ref_y);
+	data->camera.x_axis = vcross(ref_y, data->camera.z_axis);
 	data->camera.x_axis = vnormalize(data->camera.x_axis);
 	data->camera.y_axis = vcross(data->camera.x_axis, data->camera.z_axis);
 	data->camera.y_axis = vnormalize(data->camera.y_axis);
@@ -30,7 +30,7 @@ void	mrt_create_camera(t_data *data)
 	// We should not allow and FOV of 0 (focal length becomes infinite)
 	// or of 180 (focal length becomes 0), it should be 0 < FOV < 180
 	data->camera.focal_len = (float) VP_DIAG / (tanf(data->camera.fov / 2) * 2);
-	printf("%f\n", data->camera.focal_len);
 	data->camera.vp_vert_len = sqrt(pow(VP_DIAG, 2) / (pow(data->ratio, 2) + 1));
 	data->camera.vp_horiz_len = data->camera.vp_vert_len * data->ratio;
+	printf("Focal length: %f\nSensor height: %f\nSensor width: %f\n", data->camera.focal_len, data->camera.vp_vert_len, data->camera.vp_horiz_len);
 }
