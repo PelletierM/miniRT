041c5288d82542f34c0fcab878d6fa71e3e72982
Maxime Pelletier <maxpelle@c1r8p10.42quebec.com>
2023-12-11 13:24:29 -0500

    Add obj movements file

---
+++ src/obj_mvmt.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   obj_mvmt.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxpelle <maxpelle@student.42quebec.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 13:22:35 by maxpelle          #+#    #+#             */
+/*   Updated: 2023/12/11 13:24:08 by maxpelle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+void	move_obj(t_data *data, int i);
+void	rot_obj(t_data *data, int i);
+void	scale_obj(t_data *data, int i);
