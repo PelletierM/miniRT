047b8a8aef80d51d53f72fba05d092553af5f386
Maxime Pelletier <maxpelle@c1r8p10.42quebec.com>
2023-12-11 13:56:31 -0500

    Fix object movements

--- src/obj_mvmt.c
+++ src/obj_mvmt.c
@@ -6,7 +6,7 @@
 /*   By: maxpelle <maxpelle@student.42quebec.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:22:35 by maxpelle          #+#    #+#             */
-/*   Updated: 2023/12/11 13:46:36 by maxpelle         ###   ########.fr       */
+/*   Updated: 2023/12/11 13:55:08 by maxpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,19 @@ void	move_obj(t_data *data, int direction)
 	else if (data->nav_mode.obj == OBJ_CYL)
 		pos = &(data->cylinders[data->nav_mode.i].position);
 	if (direction == OBJ_MV_LEFT)
-		*pos = translate_pt(*pos, OBJ_MV_RATIO, data->camera.x_axis);
-	else if (direction == OBJ_MV_RIGHT)
 		*pos = translate_pt(*pos, -1 * OBJ_MV_RATIO, data->camera.x_axis);
+	else if (direction == OBJ_MV_RIGHT)
+		*pos = translate_pt(*pos, OBJ_MV_RATIO, data->camera.x_axis);
 	else if (direction == OBJ_MV_FORWARD)
-		*pos = translate_pt(*pos, OBJ_MV_RATIO, data->camera.z_axis);
-	else if (direction == OBJ_MV_BACK)
-		*pos = translate_pt(*pos, -1 * OBJ_MV_RATIO, data->camera.z_axis);
-	else if (direction == OBJ_MV_DOWN)
-		*pos = translate_pt(*pos, OBJ_MV_RATIO, data->camera.y_axis);
-	else if (direction == OBJ_MV_UP)
 		*pos = translate_pt(*pos, -1 * OBJ_MV_RATIO, data->camera.y_axis);
+	else if (direction == OBJ_MV_BACK)
+		*pos = translate_pt(*pos, OBJ_MV_RATIO, data->camera.y_axis);
+	else if (direction == OBJ_MV_DOWN)
+		*pos = translate_pt(*pos, -1 * OBJ_MV_RATIO, data->camera.z_axis);
+	else if (direction == OBJ_MV_UP)
+		*pos = translate_pt(*pos, OBJ_MV_RATIO, data->camera.z_axis);
+	if (data->nav_mode.obj == OBJ_CYL)
+		set_cylinders_disk(&data->cylinders[data->nav_mode.i]);
 }
 
 void	rot_obj(t_data *data, int i)
