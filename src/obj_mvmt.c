25abb7f75cebf014cee33835596292223f9099fa
Emmanuel Guefif <eguefif@c1r3p8.42quebec.com>
2023-12-11 14:07:34 -0500

    Fixed rotation

--- src/obj_mvmt.c
+++ src/obj_mvmt.c
@@ -6,7 +6,7 @@
 /*   By: maxpelle <maxpelle@student.42quebec.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:22:35 by maxpelle          #+#    #+#             */
-/*   Updated: 2023/12/11 14:01:35 by eguefif          ###   ########.fr       */
+/*   Updated: 2023/12/11 14:07:26 by eguefif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,27 +42,31 @@ void	move_obj(t_data *data, int direction)
 void	rot_obj(t_data *data, int i)
 {
 	if (data->nav_mode.obj == OBJ_CYL)
+	{
 		data->cylinders[data->nav_mode.i].orientation = get_new_orientation_rot(
 			data->cylinders[data->nav_mode.i].orientation,
 			data->camera,
 			i);
+		set_cylinders_disk(&data->cylinders[data->nav_mode.i]);
+	}
 	if (data->nav_mode.obj == OBJ_PLANE)
 		data->planes[data->nav_mode.i].orientation = get_new_orientation_rot(
 			data->planes[data->nav_mode.i].orientation,
 			data->camera,
 			i);
+
 }
 
 t_vector	get_new_orientation_rot(t_vector v, t_camera camera, int i)
 {
 	if (i == OBJ_ROT_LEFT)
-		return (rotate_figure(v, camera.z_axis, -1));
+		return (vnormalize(rotate_figure(v, camera.z_axis, -1)));
 	else if (i == OBJ_ROT_RIGHT)
-		return (rotate_figure(v, camera.z_axis, 1));
+		return (vnormalize(rotate_figure(v, camera.z_axis, 1)));
 	else if (i == OBJ_ROT_UP)
-		return (rotate_figure(v, camera.x_axis, 1));
+		return (vnormalize(rotate_figure(v, camera.x_axis, 1)));
 	else if (i == OBJ_ROT_DOWN)
-		return (rotate_figure(v, camera.x_axis, -1));
+		return (vnormalize(rotate_figure(v, camera.x_axis, -1)));
 	return (v);
 }
 
