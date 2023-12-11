c3384fb1c8f11c4dd28f1b9cd380b816366fb764
Emmanuel Guefif <eguefif@c1r3p8.42quebec.com>
2023-12-11 11:45:24 -0500

    Finished threading

--- src/main.c
+++ src/main.c
@@ -6,11 +6,14 @@
 /*   By: maxpelle <maxpelle@student.42quebec.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:10:58 by maxpelle          #+#    #+#             */
-/*   Updated: 2023/12/09 21:06:21 by eguefif          ###   ########.fr       */
+/*   Updated: 2023/12/11 11:44:17 by eguefif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+void		display_delta(struct timeval t_start);
+long int	get_time_in_micro(struct timeval t);
 
 int	main(int argc, char *argv[])
 {
@@ -27,4 +30,49 @@ int	main(int argc, char *argv[])
 	mlx_loop(data.mlx);
 	mlx_terminate(data.mlx);
 	return (0);
+}
+
+void	render(t_data *data)
+{
+	int 			i;
+	t_thread		threads[THREAD_MAX];
+
+	i = 0;
+	while (i < THREAD_MAX)
+	{
+		threads[i].id = i;
+		threads[i].data = data;
+		if (pthread_create(&threads[i].thread,
+					NULL, render_thread, &threads[i]) != 0)
+		{
+			printf("Error thread\n");
+			return ;
+		}
+		i++;
+	}	
+	i = 0;
+	while (i < THREAD_MAX)
+	{
+		pthread_join(threads[i].thread, NULL);
+		i++;
+	}
+}
+
+void	display_delta(struct timeval t_start)
+{
+	struct timeval	t_end;
+	long int		start;	
+	long int		end;	
+	float			delta;
+
+	gettimeofday(&t_end, NULL);
+	start = get_time_in_micro(t_start);
+	end = get_time_in_micro(t_end);
+	delta = (end - start) / 1000;
+	ft_printf("Rendered in : %fms\n", delta);
+}
+
+long int	get_time_in_micro(struct timeval t)
+{
+	return (t.tv_sec * 1000 * 1000 + t.tv_usec);
 }
