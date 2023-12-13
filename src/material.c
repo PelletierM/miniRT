12bdbadf4f9205e0c942861828d99018a5365fe9
Maxime Pelletier <maxpelle@c1r8p10.42quebec.com>
2023-12-13 16:02:38 -0500

    Add emissive material management

--- src/material.c
+++ src/material.c
@@ -6,7 +6,7 @@
 /*   By: maxpelle <maxpelle@student.42quebec.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 10:11:30 by eguefif           #+#    #+#             */
-/*   Updated: 2023/12/13 15:50:37 by eguefif          ###   ########.fr       */
+/*   Updated: 2023/12/13 16:00:28 by maxpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,24 @@ float	get_metallic_factor(t_data *data, t_hit hit)
 	{
 		if (data->materials[i].id == id)
 			return (data->materials[i].metallic);
+		i++;
+	}
+	return (1);
+}
+
+float	get_emissive_ratio(t_data *data, t_hit hit)
+{
+	int	i;
+	int	id;
+
+	i = 0;
+	id = get_material_id(data, hit);
+	if (id == -1)
+		return (-1);
+	while (i < data->num_materials)
+	{
+		if (data->materials[i].id == id)
+			return (data->materials[i].emissive_ratio);
 		i++;
 	}
 	return (1);
